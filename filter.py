6df643f2ff6a36420ad8503942fc2f85e9656e6d
KyreOn <kireonbk@gmail.com>
2021-11-15 15:48:04 +0500

    Revert "Шаг 1"
    This reverts commit 80eded963f65b9aad5ca11f7591d71cc8432f1ca.

--- filter.py
+++ filter.py
@@ -1,28 +1,28 @@
 from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
-img_array = np.array(img)
-height = len(img_array)
-width = len(img_array[1])
+arr = np.array(img)
+a = len(arr)
+a1 = len(arr[1])
 i = 0
-while i < height:
+while i < a - 11:
     j = 0
-    while j < width:
-        middle_brightness = 0
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                red = img_array[x][y][0]
-                green = img_array[x][y][1]
-                blue = img_array[x][y][2]
-                brightness = (int(red) + int(green) + int(blue))/3
-                middle_brightness += brightness
-        middle_brightness = int(middle_brightness // 100)
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                img_array[x][y][0] = int(middle_brightness // 50) * 50
-                img_array[x][y][1] = int(middle_brightness // 50) * 50
-                img_array[x][y][2] = int(middle_brightness // 50) * 50
+    while j < a1 - 11:
+        s = 0
+        for n in range(i, i + 10):
+            for n1 in range(j, j + 10):
+                n1 = arr[n][n1][0]
+                n2 = arr[n][n1][1]
+                n3 = arr[n][n1][2]
+                M = n1 + n2 + n3
+                s += M
+        s = int(s // 100)
+        for n in range(i, i + 10):
+            for n1 in range(j, j + 10):
+                arr[n][n1][0] = int(s // 50) * 50
+                arr[n][n1][1] = int(s // 50) * 50
+                arr[n][n1][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
-res = Image.fromarray(img_array)
+res = Image.fromarray(arr)
 res.save('res.jpg')
